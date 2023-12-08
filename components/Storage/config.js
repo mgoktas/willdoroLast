9c5d9101a1db0e6f4298d54e4843f5295a5018a7
Muhammet Rasit Goktas <103164878+mgoktas@users.noreply.github.com>
2023-12-08 15:04:04 -0800

    Update config.js

--- components/Storage/config.js
+++ components/Storage/config.js
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const uri = "mongodb+srv://mgoktashk:hkuKOChku.@cluster0.kh756qg.mongodb.net/?retryWrites=true&w=majority";
+const uri = "<YOUR-MONGODB-URI>";
 export const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 client.connect(err => {
